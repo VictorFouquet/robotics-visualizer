f6e61dcd7de11068a8511d0930e6db742749743f
victor fouquet <victor.fouquet@gmail.com>
2021-11-24 00:43:50 +0100

    Create robot instanciation

--- src/robotics/robotArmExample.h
+++ src/robotics/robotArmExample.h
@@ -4,10 +4,23 @@
 #include "matrix.h"
 #include <iostream>
 #include <stdlib.h>
+#include <vector>
 
 class RobotArm
 {
 public:
+    RobotArm() = default;
+    ~RobotArm() = default;
+
+public:
     static void forwardKinematic2DOF_DEMO();
     static void forwardKinematics6DOF(float theta1, float theta2, float theta3, float theta4, float theta5, float theta6);
+
+    Vector3d getEndEffector() const { return m_endEffector; }
+
+protected:
+    std::vector<Matrix> m_transforms;
+    std::vector<Vector3d> m_joints;
+    std::vector<Matrix> m_links;
+    Vector3d m_endEffector;
 };
