528484ca94d8154cfe956fd9bb940ad700b29be2
victor fouquet <victor.fouquet@gmail.com>
2021-11-24 16:54:17 +0100

    Update app header

--- src/application/app.h
+++ src/application/app.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "window.h"
+#include "robotArmExample.h"
+
+#include <vector>
+
 
 class App
 {
@@ -11,6 +15,15 @@ public:
     int run();
 
 private:
+    int init();
+    Frame computeFrameComponents();
+private:
     int m_windowWidth = 640, m_windowHeight = 480;
     Window m_window =  Window(m_windowWidth, m_windowHeight);
+    bool m_windowOpened = true;
+
+    std::vector<Frame> m_frames;
+    int m_frameToRender = -1;
+    
+    RobotArm* m_robot;
 };
