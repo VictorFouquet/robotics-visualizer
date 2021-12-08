e4c7491f20039b89c72fffa9e9aaaa60729d692c
victor fouquet <victor.fouquet@gmail.com>
2021-12-08 20:04:41 +0100

    Add message box to renderer

--- src/renderer/window.cpp
+++ src/renderer/window.cpp
@@ -1,11 +1,12 @@
 #include "window.h"
 #include "circle.h"
 #include "line.h"
+#include "messageBox.h"
 #include "polygon.h"
 #include "triangle.h"
 #include "vertex.h"
-
 #include <math.h>
+#include <SDL2/SDL_ttf.h>
 
 Window::Window(int width, int height) 
 {
@@ -19,6 +20,7 @@ Window::Window(int width, int height)
 bool Window::onInit() 
 {
     SDL_Init(SDL_INIT_VIDEO);
+    TTF_Init();
 
     m_window = SDL_CreateWindow(
         "Robotics Visualizer",
@@ -70,6 +72,7 @@ void Window::onLoop()
 
 void Window::onRender(Frame frame) 
 {
+
     SDL_SetRenderDrawColor(m_renderer, 10, 10, 30, 255);
     SDL_RenderClear(m_renderer);
     
@@ -93,8 +96,11 @@ void Window::onRender(Frame frame)
         circle.render(m_renderer);
     };
 
+    for (auto message : frame.getMessages())
+        message.render(m_renderer);
+    
     SDL_RenderPresent(m_renderer);
-
+    
     SDL_Delay(50);
 }
 
@@ -102,6 +108,6 @@ void Window::onExit()
 {
     SDL_DestroyWindow(m_window);
     m_window = NULL;
-
+    TTF_Quit();
     SDL_Quit();
 }
