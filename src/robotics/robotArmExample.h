433ca2d8fe77b41f232f14c378c1adc98c55b1f5
victor fouquet <victor.fouquet@gmail.com>
2021-12-11 16:34:56 +0100

    Clean robot arm classes

--- src/robotics/robotArmExample.h
+++ src/robotics/robotArmExample.h
@@ -14,9 +14,6 @@ public:
     ~RobotArm() = default;
 
 public:
-    static void forwardKinematic2DOF_DEMO();
-    static void forwardKinematics6DOF(float theta1, float theta2, float theta3, float theta4, float theta5, float theta6);
-
     Vector3d getEndEffector() const { return m_endEffector; }
 
     std::vector<std::pair<Vector3d, Vector3d>> getLinks() const { return m_links; }
