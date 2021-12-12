46dcaf463771738d9463023b203d28712912136a
victor fouquet <victor.fouquet@gmail.com>
2021-12-12 20:48:04 +0100

    Add gui component basic style getters and setters

--- src/gui/uiComponent.h
+++ src/gui/uiComponent.h
@@ -31,10 +31,19 @@ public:
     
     inline void addType(int type) { m_type |= type; }
     
+    inline void setInnerText(std::string text) { m_innerText = text; }
+    inline void setBackgroundColor(int r, int g, int b, int a) { m_backgroundColor = { r, g, b, a }; }
+    inline void setBorderColor(int r, int g, int b, int a) { m_borderColor = { r, g, b, a }; }
     inline int getType() { return m_type; }
+    inline std::string getInnerText() { return m_innerText; }
+    inline std::vector<int> getBackgroundColor(int r, int g, int b) { return m_backgroundColor; }
+    inline std::vector<int> getBorderColor(int r, int g, int b) { return m_backgroundColor; }
 
 protected:
     int m_width, m_height, m_posX, m_posY;
+    std::string m_innerText;
+    std::vector<int> m_backgroundColor{ 0, 0, 0, 0 };
+    std::vector<int> m_borderColor{ 0, 0, 0, 0 };
     std::vector<UIComponent> m_children = {};
 
     int m_type = 0;
