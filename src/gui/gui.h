eaee7a85dc1a4bb70464ead1a1c1e497aaa0fc38
victor fouquet <victor.fouquet@gmail.com>
2021-12-12 20:54:04 +0100

    Create and organize components with GUI class

--- src/gui/gui.h
+++ src/gui/gui.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <string>
+
 #include "uiComponent.h"
+
 
 class GUI
 {
@@ -8,8 +11,25 @@ public:
     GUI() = default;
     ~GUI() = default;
 
-    inline std::vector<UIComponent> getComponents() { return m_components; }
-    inline void appendComponent(UIComponent component) { m_components.push_back(component); }
+    void init(int width, int height);
+    
+    void onEvent(AppEvent e);
+
+    inline UIComponent getMainContainer() { return m_mainContainer; }
+    // inline std::vector<UIComponent> getComponents() { return m_components; }    
+    inline std::vector<UIComponent> getComponents() { return m_mainContainer.getChildren(); }
+
+    // inline void appendComponent(UIComponent component) { m_mainContainer.appendChild(component); }
+    inline void appendComponent(UIComponent component) { m_mainContainer.appendChild(component); }
+
+    inline std::vector<UIComponent*> getButtons(); // { return m_buttons; }
+    std::vector<UIComponent> getButtons(UIComponent rootComponent, std::vector<UIComponent> buttons = {});
+
+    UIComponent createButton(std::string text, int x, int y, int w, int h);
+    UIComponent createContainer(int x, int y, int w, int h);
 private:
     std::vector<UIComponent> m_components = {};
+    UIComponent m_mainContainer;
+    std::vector<UIComponent> m_buttons = {};
+    std::vector<UIComponent> m_containers = {};
 };
